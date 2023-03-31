5cfa8daa4518d622e84f6d2ced064dc45545a71f
LucasKleaL <lucaskleal222@outlook.com>
2023-03-31 14:36:57 -0300

    Adicionado CadastroApi
    -Adicionado configurações de cors, routes e padrão json no index.ts.
    -Adicionado model User.
    -Adcionado UserRepository com a função básica de add.
    -Adicionado UserRouter com o endpoint de add.
    -Corrigido admin.ts e firebase.ts.

---
+++ server/models/User.ts
@@ -0,0 +1,10 @@
+type User = {
+    uid: string,
+    email: string,
+    password: string,
+    name: string,
+    cpf: string,
+    telephone_number: string
+};
+
+export default User;
--- server/util/admin.ts
+++ server/util/admin.ts
@@ -5,9 +5,8 @@ dotenv.config();
 
 const firebaseConfig = JSON.parse(process.env.FIREBASE_ADMIN_KEY || "{}");
 
-admin.initializeApp({
+export const firebaseAdmin = admin.initializeApp({
     credential: admin.credential.cert(firebaseConfig)
 });
 
-const db = admin.firestore();
-module.exports = { admin, db }
+export const db = admin.firestore();
