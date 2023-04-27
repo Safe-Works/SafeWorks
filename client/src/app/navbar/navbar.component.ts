8dc61e2d2518d159997a5da692e17ce0feeb7888
Tiago <tiagofelipemuller@gmail.com>
2023-04-27 01:27:15 -0300

    Adicionando arquivos que faltaram

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { UserAuth } from '../auth/User.Auth';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  @Output() logout = new EventEmitter<void>();
+
+  constructor(public userAuth: UserAuth) {}
+
+  ngOnInit(): void {
+    this.userAuth.authUserFromToken();
+  }
 
 }
