c570940be55ec39c67f3bcac61218b612e7b0b7a
Vitor Felix <vitorfelix37@gmail.com>
2023-05-27 22:27:05 -0300

    ajustes de estilo

--- client/src/app/models/job-advertisement.model.ts
+++ client/src/app/models/job-advertisement.model.ts
@@ -2,19 +2,18 @@ class JobAdvertisement {
   constructor(
     public user: string[],
     public title: string,
-    public category: string,
-    public price: number,
-    public priceType: string, // Tipo de valor (Hora, metro, dias, quantidade)
-    public location: string,
     public description: string,
-    public id?: string,
+    public category: string,
+    public location: string,
+    public price: number,
+    public price_type: string, // Tipo de valor (Hora, metro, dias, quantidade)
+    public displacement_fee?: number, // Custo de deslocamento
+    public uid?: string,
+    public delivery_time?: string, // Tempo de entrega, se aplicável
     public media?: string[], // Lista de imagens do anúncio
-    public deliveryTime?: string, // Tempo de entrega, se aplicável
-    public createdAt?: Date,
-    public updatedAt?: Date,
-    public isExpired?: boolean,
-    public isSold?: boolean,
-    public displacementFee?: number, // Custo de deslocamento
+    public created_at?: Date,
+    public updated_at?: Date,
+    public is_expired?: boolean,
   ) { }
 }
 
