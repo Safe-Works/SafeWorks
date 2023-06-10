0d8192213523e0f8430e0a9901a9e9ffb32faea2
Vitor Felix <vitorfelix37@gmail.com>
2023-06-09 23:43:57 -0300

    página de listar anúncios e endpoints de get por id, all paginado e delete

---
+++ client/src/app/components/posts/view-post/view-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-post',
+  templateUrl: './view-post.component.html',
+  styleUrls: ['./view-post.component.css']
+})
+export class ViewPostComponent {
+
+}
