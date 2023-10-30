ebac1b632e65bd450ba76857e817f64a7edfd355
LucasKleaL <63320965+LucasKleaL@users.noreply.github.com>
2023-10-30 20:44:00 -0300

    Adicionado estrutura de Complaints
    -Adicionado model, controller, repository e router de Complaint.
    -Adicionado model Complaint no client.

---
+++ server/src/controllers/ComplaintsController.ts
@@ -0,0 +1,9 @@
+import ComplaintsRepository from "../repositories/ComplaintsRepository";
+
+const complaintsRepository = new ComplaintsRepository();
+
+class ComplaintsController {
+
+}
+
+export default ComplaintsController;
